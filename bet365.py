e12436bd2535d486f67b47dc4f1bda6319d42a79
liufufeng404 <135004836+liufufeng404@users.noreply.github.com>
2023-06-21 21:26:34 +0800

    Create bet365.py

---
+++ bet365.py
@@ -0,0 +1,13 @@
+
+'''bet365_bot
+
+www.bet365.com www.365-288.com
+soccer in play： http://43.159.200.98/api/bet365/?sportId=1&isRB=1
+
+Real time updates of the project, providing real-time data
+
+
+Please contact us for cooperation: ichuan2062@gmail.com
+TG https://t.me/iMoFei
+
+bet365 bot specializes in risk control and anti crawling, and there is no data that cannot be crawled! '''
