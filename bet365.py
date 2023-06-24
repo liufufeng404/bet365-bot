aadd6686b3469484f1a97410cbefefc9baf13a48
liufufeng404 <135004836+liufufeng404@users.noreply.github.com>
2023-06-24 15:34:39 +0800

    Update bet365.py

--- bet365.py
+++ bet365.py
@@ -1,5 +1,5 @@
 
-'''bet365_bot
+keys_words = 'bet365_bot
 
 www.bet365.com www.365-288.com
 soccer in play： http://43.159.200.98/api/bet365/?sportId=1&isRB=1
@@ -10,4 +10,9 @@ Real time updates of the project, providing real-time data
 Please contact us for cooperation: ichuan2062@gmail.com
 TG https://t.me/iMoFei
 
-bet365 bot specializes in risk control and anti crawling, and there is no data that cannot be crawled! '''
+bet365 bot specializes in risk control and anti crawling, and there is no data that cannot be crawled! '
+
+urls=  [
+  'www.bet365.com',
+  'www.365-288.com',
+]
